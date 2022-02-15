c376a8f16c8e82a53290deb7166e8b94f99db002
Daniel Tedman <dbtedman@gmail.com>
2022-02-15 20:09:33 +1000

    Add basic Spring Boot application with pipeline

---
+++ src/main/java/com/danieltedman/hoist/HoistApplication.java
@@ -0,0 +1,13 @@
+package com.danieltedman.hoist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danieltedman/hoist/HoistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danieltedman.hoist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
